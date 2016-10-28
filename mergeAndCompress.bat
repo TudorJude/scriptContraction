cbf8a77b8b654ad7ee1b543df0178e896e681283
Fratean Tudor <fratean.tudor@gmail.com>
2016-10-28 15:11:55 +0300

    AddExperience v6

--- mergeAndCompress.bat
+++ mergeAndCompress.bat
@@ -1,4 +1,6 @@
+:loop
 copy NUL unified.txt
 for /R src %%f in (.\*) do type "%%f" >> unified.txt
 java -jar closureCompiler.jar --js unified.txt --js_output_file cloudScript-constricted.js
 pause
+goto loop
