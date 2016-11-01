20c30223f68ae85780b7ca140d9ee0a312052c5f
Fratean Tudor <fratean.tudor@gmail.com>
2016-11-01 14:20:00 +0200

    Splitted upgradePart and upgradeCar in new files

--- mergeAndCompress.bat
+++ mergeAndCompress.bat
@@ -1,6 +1,6 @@
 :loop
+pause
 copy NUL unified.txt
 for /R src %%f in (.\*) do type "%%f" >> unified.txt
 java -jar closureCompiler.jar --js unified.txt --js_output_file cloudScript-constricted.js
-pause
 goto loop
