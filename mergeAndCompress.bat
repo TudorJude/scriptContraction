4ff47a36d583e15f8d674cccbdc8031307fe811c
deadPool47 <tudor.jude47@gmail.com>
2017-02-17 16:34:37 +0200

    uncompressed code

--- mergeAndCompress.bat
+++ mergeAndCompress.bat
@@ -1,6 +1,6 @@
 :loop
 pause
-copy NUL unified.txt
-for /R src %%f in (.\*) do type "%%f" >> unified.txt
-java -jar closureCompiler.jar --js unified.txt --js_output_file cloudScript-constricted.js
+copy NUL unified.js
+for /R src %%f in (.\*) do type "%%f" >> unified.js
+java -jar closureCompiler.jar --js unified.js --js_output_file cloudScript-constricted.txt
 goto loop
